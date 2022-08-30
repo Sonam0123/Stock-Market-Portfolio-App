01965ccd80ff43e6b295058ef64b123c1426899f
sonam <tseringsonam34@hotmail.com>
2022-08-30 15:01:57 -0400

    rendering api data on page

--- stocksportfolio/robbinghoods/views.py
+++ stocksportfolio/robbinghoods/views.py
@@ -2,18 +2,20 @@ from django.shortcuts import render
 
 def home(request):
 
-	import request
+	import requests
 	import json
 
-	# token: pk_d91740a399c34554bda6f3ba376d9202
-	api_request = request.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_d91740a399c34554bda6f3ba376d9202")
+	if request.method == 'POST':
+		ticker = request.POST['ticker']
+		api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_d91740a399c34554bda6f3ba376d9202")
 
-	try:
-		api = json.loads(api_request.content)
-	except Exception as e:
-		api = "error..."
-
-	return render(request, 'home.html', {})
+		try:
+			api = json.loads(api_request.content)
+		except Exception as e:
+			api = "error..."
+		return render(request, 'home.html', {'api': api})
+	else :
+		return render(request, 'home.html', {'ticker': 'Enter a Ticker symbol above'})
 
 
 def about(request):
