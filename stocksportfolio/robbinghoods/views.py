5c9b8775c15932017851a33bf51c14ad08b5deee
sonam <tseringsonam34@hotmail.com>
2022-08-30 14:00:35 -0400

    making api requests

--- stocksportfolio/robbinghoods/views.py
+++ stocksportfolio/robbinghoods/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 
 def home(request):
+
+	import request
+	import json
+
+	# token: pk_d91740a399c34554bda6f3ba376d9202
+	api_request = request.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_d91740a399c34554bda6f3ba376d9202")
+
+	try:
+		api = json.loads(api_request.content)
+	except Exception as e:
+		api = "error..."
+
 	return render(request, 'home.html', {})
 
 
